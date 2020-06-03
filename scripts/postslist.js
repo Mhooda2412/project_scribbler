b1cdf86f8390ca4f41efbf92e51bb70cd07f0eb6
Manish hooda <mhooda2412@gmail.com>
2020-06-03 18:13:45 +0530

    Delete post functionality added

---
+++ scripts/postslist.js
@@ -0,0 +1,12 @@
+// Delete post on click of yes in modal
+$(DeleteModal).on('show.bs.modal',function (event){
+    var button = $(event.relatedTarget)
+    var id = button.data('id')
+    var modal = $(this)
+
+    modal.find('.btn-success').on('click',function(){
+        var post = document.getElementById(id);
+        post.style.display='none'    
+    })
+    
+})

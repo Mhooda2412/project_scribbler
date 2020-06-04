9eee6099fbfda0d9a052308ee1d17071436b8b16
Manish hooda <mhooda2412@gmail.com>
2020-06-04 21:01:13 +0530

    Post content and edit function added

--- scripts/post.js
+++ scripts/post.js
@@ -2,25 +2,41 @@ function editMode(){
     var para = document.getElementById('postTitlePara')
     var input = document.getElementById('postTitleInput')
     var button= document.getElementById('editButton')
+    var textareaDev = document.querySelector(".postContentInput")
+    var contentPara = document.getElementById('postContentid') 
+    var textarea=document.getElementById('postContentInputId')
  
     
 
     if(button.textContent.match('Edit')){
-        if(input.style.display === 'none'){
+        if(input.style.display === 'none' && textareaDev.style.display==='none'){
             para.style.display='none'
             input.style.display='block'
             input.value=para.textContent
+            contentPara.style.display='none'
+            textarea.innerHTML=contentPara.textContent.replace(/\s+/g,' ').trim()
+            textareaDev.style.display='block'
+            textarea.style.height='auto'
+            textarea.style.height=textarea.scrollHeight+'px'
+
         }
         button.innerHTML= 'Save '+'<i class="fa fa-floppy-o" aria-hidden="true"></i>'       
     }
 
     else if(button.textContent.match('Save')){
         button.innerHTML= 'Edit '+'<i class="fa fa-pencil-square-o" aria-hidden="true"></i>'
-        if(input.style.display === 'block'){
+        if(input.style.display === 'block' && textareaDev.style.display==='block'){
             para.style.display='block'
             input.style.display='none'
             para.textContent=input.value
+
+            contentPara.style.display="block"
+            contentPara.textContent = textarea.value
+            textareaDev.style.display="none"
+
         }
     }
 
 }
+
+
